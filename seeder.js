18984af7db043e88d9c3e263625d524874f73482
thiagoaugusto <thiago.android88@gmail.com>
2021-04-03 18:15:57 -0300

    Bootcamp & User Relationship

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 connectDB();
@@ -23,11 +24,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import data
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data Imported...'.green.inverse);
 
     process.exit();
@@ -41,6 +47,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log('Data Destroyed...'.red.inverse);
     process.exit(1);
