b963eefcf7900d62aed3675b4268fc5ac2545363
thiagoaugusto <thiago.android88@gmail.com>
2021-04-07 13:32:36 -0300

    Logout to Clear Token Cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,14 +11,16 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
-  //   else if (req.cookies.token) {
-  //     token = req.cookies.token;
-  //   }
+  // // Set token from cookie
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
 
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
@@ -28,7 +30,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,13 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
+  logout,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
