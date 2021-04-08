ae0cfbd8bbaac237b0e8cc2708d31d7f1e6f66cf
thiagoaugusto <thiago.android88@gmail.com>
2021-04-07 22:22:28 -0300

    XSS Protection & Security Headers

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const fileupload = require('express-fileupload');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
 
 // Load env vars
@@ -39,6 +41,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
