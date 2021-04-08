d4926cb9044d23f01c0c3a155561afae0bd9c3a7
thiagoaugusto <thiago.android88@gmail.com>
2021-04-07 23:23:38 -0300

    Rate Limiting, HPP & CORS

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const fileupload = require('express-fileupload');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 // Load env vars
@@ -47,6 +50,20 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 10,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enabled CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
